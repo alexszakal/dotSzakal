3adbd670f8b910638b53a014038b80aa89ba172e
Alex Szakál <szakal.alex@gmail.com>
2023-04-20 19:25:19 +0200

    New keymaps file added to the repo

---
+++ .config/nvim/lua/szakal/keymaps.lua
@@ -0,0 +1,114 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts) -- Keep the copied text in buffer when paste and change to a highlighted text
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+keymap("n", "<leader>pv", ":Ex<CR>", opts)
+
+-- Keybindingd from ThePrimeagen
+
+-- Move the highlighted text in visual mode
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") --Comment out temporarily
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Append the next line to the current line but keep the position of the cursor
+vim.keymap.set("n", "J", "mzJ`z")
+-- Keep the cursor in the center while half page jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Search terms stay in the middle vertically
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Set <leader>-y to yank text to system clipboard instead of nvim clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Delete into void register
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- Change project using tmux NOT WORKING FOR SOME REASON
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- Quickfix navigation through the quickfix list
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")  --Commented out because this is used now for window navigation
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")  --Commented out because this is used now for window navigation
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Change the word the cursor is currently staying at
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Add executable permissions to the current file
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Jump to packer.lua file 
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/szakal/packer.lua<CR>");
+
+-- Source the current file
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+
