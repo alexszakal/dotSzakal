f93f433c2fc15a1042ac5b74ad9ef86b63ddb944
Alex Szakál <szakal.alex@gmail.com>
2023-06-14 06:27:02 +0200

    Telescope path_display smart -> truncated to speed up

--- .config/nvim/lua/szakal/telescope.lua
+++ .config/nvim/lua/szakal/telescope.lua
@@ -12,7 +12,7 @@ telescope.setup {
 
     prompt_prefix = " ",
     selection_caret = " ",
-    path_display = { "smart" },
+    path_display = { "truncated" }, -- This used to be "smart"
 
     mappings = {
       i = {
