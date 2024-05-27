f5c9bce7d46d865370762cc2cdc5286b2d110d44
Szakal Alex <alex.szakal@mediso.com>
2024-05-27 09:39:47 +0200

    Update keymaps for Telescope

--- .config/nvim/lua/szakal/keymaps.lua
+++ .config/nvim/lua/szakal/keymaps.lua
@@ -118,7 +118,7 @@ end)
 keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 -- keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<leader>g", "<cmd>Telescope git_files<cr>", opts)
-keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<C-t>",     "<cmd>Telescope live_grep<cr>", opts)
 
 -- Debugging
 vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
